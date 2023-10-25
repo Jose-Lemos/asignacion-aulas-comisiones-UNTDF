749b8fc70c08ee6a45e8b44319119dad30d24efd
José Lemos <90288108+Jose-Lemos@users.noreply.github.com>
2023-10-24 21:21:26 -0300

    Update admin.py
    Actualizamos el admin, registrando los modelos para poder hacer el ABML de los datos con el admin de Django

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from app.models import Herramienta, Aula, Carrera, Materia, Comision, Comision_BH, Asignacion
 
 # Register your models here.
+admin.site.register(Herramienta)
+admin.site.register(Aula)
+admin.site.register(Carrera)
+admin.site.register(Materia)
+admin.site.register(Comision)
+admin.site.register(Comision_BH)
+admin.site.register(Asignacion)
