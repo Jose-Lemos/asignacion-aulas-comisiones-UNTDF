1d86f6d285de9ddff3b351f0c6626771325c7656
knbnunez <knbnunez@gmail.com>
2023-11-25 12:59:54 -0300

    Se corrigió lógica y sintaxis de la sentencia que trae las aulas libres

--- app/views/asignar_automaticamente.py
+++ app/views/asignar_automaticamente.py
@@ -29,9 +29,11 @@ class AsignarAutomaticamenteView(TemplateView):
         for c in comisiones_sin_asignar: 
             # print(c)
 
-            ############################
+            # BEGIN, es solamente para setear la variable Booleana correspondiente, no me interesa lo que devuelva en sí
+            # ---
+            ###########################
             # Caso preferencia de Aula:
-            ############################
+            ###########################
             espacio_aula_comision = Espacio_Aula.objects.raw(f"""
                 select distinct aea.*
                 from app_espacio_aula aea 
@@ -41,9 +43,9 @@ class AsignarAutomaticamenteView(TemplateView):
             quiere_aula_exclusiva=False
             if len(espacio_aula_comision) > 0: quiere_aula_exclusiva=True
             
-            ############################
+            #############################
             # Caso herramientas Comision:
-            ############################
+            #############################
             herramienta_comision = Comision.objects.raw(f"""
                 select *
                 from app_comision ac 
@@ -52,50 +54,51 @@ class AsignarAutomaticamenteView(TemplateView):
             """)
             quiere_herramienta=False
             if len(herramienta_comision) > 0: quiere_herramienta=True
+            # ---
+            # END
 
             # Franjas horarias de comisiones sin asignar:
             bhs_sin_asignar = Comision_BH.objects.filter(comision_id=c.nombre)
             
             for bh in bhs_sin_asignar:
-                # print(bh)
-                
                 default_query = f"""
-                    select ea.id 
+                    select ea.* 
                     from app_espacio_aula ea
-                    where ea.id not in (
-                        select eax.id
+                    where ea.nombre_combinado not in (
+                        select eax.nombre_combinado
                         from app_asignacion ag
                         inner join app_comision_bh cbh on cbh.id = ag.comision_bh_id 
                         inner join app_espacio_aula eax on eax.id = ag.espacio_aula_id 
-                        where { bh.hora_ini } between cbh.hora_ini and cbh.hora_fin
+                        where '{ bh.hora_ini }' between cbh.hora_ini and cbh.hora_fin
                     )
-                    and ea.id = (
-                        select eax.id
+                    and ea.nombre_combinado = (
+                        select eax.nombre_combinado
                         from app_espacio_aula eax
                         inner join app_aula ax on ax.id = eax.aula_id 
-                        where eax.capacidad_total >= { c.cant_inscriptos }
+                        where eax.capacidad_total >= { c.cant_insc }
                         group by eax.nombre_combinado 
                         order by eax.capacidad_total 
-                        limit 1
+                        limit 1 -- Con esto queda expresada la limitación de Aulas que nos va a devolver, 
+                                -- puede que sea un aula combinada, pero estaremos hablando de "1 instancia" en concreto.
                     ) 
                 """
-
+                
                 # FALTA TESTEAR
                 if quiere_aula_exclusiva:
                     aula_libre = Espacio_Aula.objects.raw(f"""
                         { default_query }
-                        and ea.id = (
-                            select eax.id
+                        and ea.nombre_combinado = (
+                            select eax.nombre_combinado
                             from app_espacio_aula eax
                             inner join app_aula ax on ax.id = eax.aula_id 
-                            where ax.id = { bh.comision.materia.carrera.aula_exclusiva }
+                            where ax.id = { bh.comision.aula_exclusiva }
                         )
                     """)
                 elif quiere_herramienta:
                     aula_libre = Espacio_Aula.objects.raw(f"""
                         { default_query }
-                        and ea.id = (
-                            select eax.id
+                        and ea.nombre_combinado = (
+                            select eax.nombre_combinado
                             from app_espacio_aula eax
                             inner join app_aula ax on ax.id = eax.aula_id 
                             inner join app_aula_herramientas ah on ah.aula_id = ax.id 
@@ -109,91 +112,24 @@ class AsignarAutomaticamenteView(TemplateView):
                 else:
                     aula_libre = Espacio_Aula.objects.raw(f"{ default_query }")
                 
+                
+                # ATENTOS: la única manera que tuve de ver los atributos de lo que retorna la query es metiéndola en un FOREACH, aunque haya una sola fila.
+                # for a in aula_libre: print(a.id, a.nombre_combinado)
+
+                # Luego de realizar la consulta correspondiente, consultamos si hubo un aula libre
                 if aula_libre: # INSERT ASIGNACION TABLE
-                    # se puede hacer con ORM
-                    Asignacion.objects.raw(f"""
-                        insert into app_asignacion(comision_bh_id, espacio_aula_id)
-                        values ( { bh.id }, { aula_libre.id })
-                    """)
+                    # Si es un Aula Combinada, traerá más de un registro. Es necesario bloquear todas las aulas relacionadas a esta combinación para que no sean tenidas en cuenta al momento de consultar por Aulas Libres
+                    for a in aula_libre: 
+                        
+                        # DESCOMENTAR, ES LA ASIGNACIÓN EN SÍ!
+                        # se puede hacer con ORM:
+                        # Asignacion.objects.raw(f"""
+                        #     insert into app_asignacion(comision_bh_id, espacio_aula_id)
+                        #     values ( { bh.id }, { a.id })
+                        # """)
+
+                        print(f"Asignación realizada { c.nombre }; { a.nombre_combinado }")
+                #
+                else:   print(f"No había aula disponible para { c.nombre }")
 
         return render(request, self.template_name)
-
-    # def asignar_automaticamente(request):
-    #     comisiones_asignadas = 0
-    #     comisiones_no_asignadas = 0
-
-    #     comisiones = Comision.objects.all()
-
-    #     for comision in comisiones:
-    #         #asignaciones_comision = Comision_BH.objects.filter(comision=comision)
-
-    #         herramientas_necesarias = comision.preferencias.all()
-    #         # Obtener las IDs de las asignaciones de Comision_BH para la comisión actual
-    #         comision_bh = Comision_BH.objects.filter(comision=comision).first()
-
-    #         if comision_bh:
-    #             dia = comision_bh.dia
-    #             hora_ini = comision_bh.hora_ini
-    #             hora_fin = comision_bh.hora_fin
-    #             fecha_ini = comision_bh.fecha_ini
-    #             fecha_fin = comision_bh.fecha_fin
-    #     # Obtener las IDs de las asignaciones para la comisión actual
-    #         ids_asignaciones_comision = Comision_BH.objects.filter(comision=comision).values_list('id', flat=True)
-
-    #     # Filtrar las aulas disponibles excluyendo aquellas que tienen asignaciones que coinciden con las horas de las comisiones
-    #         aulas_disponibles = Espacio_Aula.objects.exclude(
-    #         asignacion__comision_bh__id__in=Subquery(ids_asignaciones_comision),
-    #         asignacion__comision_bh__hora_ini__lte=F('asignacion__comision_bh__hora_fin'),
-    #         asignacion__comision_bh__hora_fin__gte=F('asignacion__comision_bh__hora_ini')
-    #     )
-
-
-    #         # Filtrar aulas disponibles que tengan todas las herramientas necesarias
-    #         aulas_con_herramientas = list([1])
-    #         for aula in aulas_disponibles:
-    #             herramientas_aula = aula.aula.herramientas.all() if aula.aula else []
-    #             if all(herramienta in herramientas_aula for herramienta in herramientas_necesarias):
-            
-    #               aulas_con_herramientas.append(aula)
-
-
-    #         capacidad_requerida = comision.cant_insc
-    #         aula_asignada = None
-
-    #         # Filtrar aulas por capacidad
-    #         aulas_con_capacidad = aulas_disponibles.filter(capacidad_total__gte=capacidad_requerida)
-    #         if aulas_con_capacidad.exists():
-    #             # Verificar si el aula tiene las herramientas necesarias
-    #             for aula in aulas_con_capacidad:
-    #                 herramientas_necesarias = comision.preferencias.all()
-    #                 if herramientas_necesarias and aula.aula.herramientas.filter(id__in=herramientas_necesarias.values_list('id', flat=True)).count() == len(herramientas_necesarias):
-    #                     aula_asignada = aula
-    #                     break
-
-    #         if not aula_asignada:
-    #             # Si no se encontró un aula con todas las herramientas necesarias, se elige una por capacidad más cercana
-    #             aula_asignada = aulas_disponibles.order_by('capacidad_total').first()
-
-    #         if aula_asignada:
-    #             # Verificar si la comisión requiere aula exclusiva
-    #             if comision.requiere_aula_exclusiva and comision.materia.first().carrera.aula_exclusiva:
-    #                 aula_asignada = comision.materia.first().carrera.aula_exclusiva.aula
-
-    #             # Asignar el aula a la comisión
-    #             Asignacion.objects.create(
-    #                 espacio_aula=aula_asignada,
-    #                 comision_bh = Comision_BH.objects.create(
-    #                     comision=comision,
-    #                     dia=dia,  # Esto puede ser un str, asegúrate de que sea un str con el formato correcto
-    #                     hora_ini=hora_ini,  # Esto también debe ser un str con el formato correcto
-    #                     hora_fin=hora_fin,  # Igualmente, un str con el formato correcto
-    #                     fecha_ini=fecha_ini,  # Un str con el formato correcto
-    #                     fecha_fin=fecha_fin  # También, un str con el formato correcto
-    # )
-    #             )
-    #             comisiones_asignadas += 1
-    #         else:
-    #             comisiones_no_asignadas += 1
-
-    #     # Mostrar el resultado
-    #     return HttpResponse(f'Comisiones asignadas: {comisiones_asignadas}<br>Comisiones no asignadas: {comisiones_no_asignadas}')
